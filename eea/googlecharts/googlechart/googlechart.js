38be16e1a5bc9765094b49628ace720386fa7a26
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-12-21 15:20:02 +0000

    Refs #4844; Initial checkin

---
+++ eea/googlecharts/googlechart/googlechart.js
@@ -0,0 +1,22 @@
+google.load('visualization', '1.0');
+function drawVisualization(parameters, container_id) {
+    var wrapper = new google.visualization.ChartWrapper(parameters);
+    wrapper.setContainerId(container_id);
+    wrapper.draw();
+}
+
+function loadJSON(settings_and_data_url, container_id){
+    jQuery.ajax({
+        url: settings_and_data_url,
+        success: function(data){
+            drawVisualization (data, container_id);
+        }
+    });
+}
+
+function drawChart(settings_and_data_url, container_id){
+    google.setOnLoadCallback(function(){
+        loadJSON(settings_and_data_url, container_id);
+    });
+}
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+""" EEA GoogleCharts Installer
+"""
+from setuptools import setup, find_packages
+import os
+
+NAME = 'eea.googlecharts'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+setup(name=NAME,
+      version=VERSION,
+      description="Wrapper for Google Charts Api",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea charts google zope plone',
+      author='Zoltan Szabo, European Environment Agency',
+      author_email='webadmin@eea.europa.eu',
+      url='http://svn.eionet.europa.eu/projects/'
+          'Zope/browser/trunk/eea.googlecharts',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['eea'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
