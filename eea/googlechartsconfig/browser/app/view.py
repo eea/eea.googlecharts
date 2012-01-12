e0a39b049da661edcc9c851b168973e4950d1c13
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-01-12 17:48:14 +0000

    Fixes #4873   - Cleanup overrides code for daviz view as this functionality is now provided by eea.daviz

--- eea/googlechartsconfig/browser/app/view.py
+++ eea/googlechartsconfig/browser/app/view.py
@@ -1,31 +1,7 @@
-# -*- coding: utf-8 -*-
 """ Module that contains default view
 """
-__author__ = """European Environment Agency (EEA)"""
-__docformat__ = 'plaintext'
-__credits__ = """contributions: Alin Voinea"""
-
 from eea.daviz.browser.app import view
 
 class View(view.View):
     """ Default view
     """
-    @property
-    def viewCategories(self):
-        """ Returns view categories
-        """
-        views = self.accessor.views
-
-        categories = []
-        for v in views:
-            if v.get('name').split('.')[0] not in categories:
-                categories.append(v.get('name').split('.')[0])
-        return categories
-
-    def get_viewsforcategory(self, category):
-        """ Returns views for a category
-        """
-        views = self.accessor.views
-        views = [v.get('name') for v in views if v.get('name').split('.')[0] == category]
-        return views
-
