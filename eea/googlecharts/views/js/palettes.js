76768ebde82a4413322fe246b60392f5141f5cf9
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-21 10:00:56 +0000

    Refs #4988; Added eea gradient colours palettes

--- eea/googlecharts/views/js/palettes.js
+++ eea/googlecharts/views/js/palettes.js
@@ -1,6 +1,7 @@
 var chartPalettes = {"default":{"name":"Default Palette",
                                 "colors":
-                                    ["#AFBC21",  //pantone 583
+                                    [
+                                    "#AFBC21",  //pantone 583
                                     "#002C56",  //85% pantone 296
                                     "#6b7427",  //pantone 385
                                     "#844980",  //pantone 682
@@ -24,8 +25,56 @@ var chartPalettes = {"default":{"name":"Default Palette",
                                     "#eaeaea"  //pantone cool grey 2
                                     ]
                                 },
-                    "palette1":{"name":"Gradien Palette",
+                    "company":{"name":"Company Colors",
                                 "colors":
-                                    ["#AABBCC","#DDEEFF","#001122","#334455"]
+                                    [
+                                    "#AFBC21",  //pantone 583
+                                    "#002C56"  //85% pantone 296
+                                    ]
+                                },
+                    "green":{"name":"Green Gradient",
+                                "colors":
+                                    [
+                                    "#6b7427",  //pantone 385
+                                    "#8a9c3a",  //pantone 582
+                                    "#b4cb4c",  //pantone 584
+                                    "#c0cf99"  //pantone 7492
+                                    ]
+                                },
+                    "red":{"name":"Red Gradient",
+                                "colors":
+                                    [
+                                    "#844980",  //pantone 682
+                                    "#b8006b",  //pantone 227
+                                    "#ac003e",  //pantone 152
+                                    "#d67c9d"  //pantone 7432
+                                    ]
+                                },
+                    "yellow":{"name":"Yellow Gradient",
+                                "colors":
+                                    [
+                                    "#d48625",  //pantone 152
+                                    "#e3b73b",  //pantone 7409
+                                    "#efdc1e",  //pantone 109
+                                    "#fffabe"  //pantone 601
+                                    ]
+                                },
+                    "blue":{"name":"Blue Gradient",
+                                "colors":
+                                    [
+                                    "#51697a",  //pantone 5405
+                                    "#6c8ba2",  //pantone 549
+                                    "#9cc9c8",  //pantone 570
+                                    "#c7e0e9"  //pantone 317
+                                    ]
+                                },
+                    "gray":{"name":"Gray Gradient",
+                                "colors":
+                                    [
+                                    "#000000",  //black
+                                    "#7c7c7c",  //pantone cool grey 9
+                                    "#c0c0c0",  //pantone cool grey 5
+                                    "#eaeaea"  //pantone cool grey 2
+                                    ]
                                 }
                     };
