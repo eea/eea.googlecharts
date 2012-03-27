44dabae69659d45a349e5768cd085c48aa6c569f
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-27 15:36:42 +0000

    Refs #4949  - Automatically add Charts as a view for Daviz and create a default table view with all columns

--- eea/googlecharts/views/events.py
+++ eea/googlecharts/views/events.py
@@ -1,8 +1,97 @@
 """ Handle events
 """
+import logging
+import json
+from zope.component import getUtility, queryAdapter, getMultiAdapter
+from eea.app.visualization.interfaces import IVisualizationConfig
 from eea.app.visualization.views.events import facet_deleted
+from zope.schema.interfaces import IVocabularyFactory
+logger = logging.getLogger('eea.googlecharts')
 
 def googlechart_facet_deleted(obj, evt):
     """ Cleanup removed facet from view properties
     """
     return facet_deleted(obj, evt, 'googlechart.googlecharts')
+
+def create_default_views(obj, evt):
+    """ Create default views
+    """
+    mutator = queryAdapter(obj, IVisualizationConfig)
+    if not mutator:
+        logger.warn("Couldn't find any IVisualizationConfig adapter for %s",
+                    obj.absolute_url(1))
+        return
+
+    # Views already configure, do nothing
+    if mutator.views:
+        return
+
+    vocab = getUtility(IVocabularyFactory,
+                       name="eea.daviz.vocabularies.FacetsVocabulary")
+    columns = vocab(obj)
+
+    # If the table is empty, do nothing
+    if not len(columns):
+        return
+
+    mutator.add_view('googlechart.googlecharts')
+
+    request = getattr(obj, 'REQUEST', None)
+    if not request:
+        return
+
+    # Add default charts
+    chart = {
+        'id': 'chart_1',
+        'name': 'Chart',
+        'width': "800",
+        'height': "600",
+        'filters': "{}",
+        'filterposition': "0",
+        'isThumb': False,
+        'dashboard': {},
+        'config': {
+            u'chartType': u'Table',
+            u'options': {u'legend': u'none', u'title': u'Chart'},
+            u'dataTable': []
+            },
+        'options': {
+            u'showChartButtons': False,
+            u'fontName': u'Verdana',
+            u'fontSize': 12,
+            u'state': u'{"showTrails":false}'
+        },
+        'columns': {'original': [], 'prepared': []},
+    }
+
+    # Config to JSON
+    chart['config'] = json.dumps(chart['config'])
+
+    # Options to JSON
+    chart['options'] = json.dumps(chart['options'])
+
+    # Add table columns
+    for term in columns:
+        original = {
+            "name": term.value,
+            "status": 1
+        }
+        chart['columns']['original'].append(original)
+        prepared = {
+            'name': term.value,
+            'status': 1,
+            'fullname': term.title
+        }
+        chart['columns']['prepared'].append(prepared)
+
+    chart['columns'] = json.dumps(chart['columns'])
+
+    # Add chart
+    query = {'charts': [chart]}
+    query = json.dumps(query)
+
+    request.form['charts'] = query
+    submit_charts = getMultiAdapter((obj, request),
+                                    name=u'googlechart.submit_charts')
+    submit_charts()
+    request.form.pop('charts')
