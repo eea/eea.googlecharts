5bb582a205e6d9dfc3c236299cfdaf7c48e2e794
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-29 08:04:03 +0000

    Fixed jslints

--- eea/googlecharts/views/js/palettes.js
+++ eea/googlecharts/views/js/palettes.js
@@ -7,17 +7,15 @@ if (!Array.prototype.indexOf)
     var len = this.length >>> 0;
 
     var from = Number(arguments[1]) || 0;
-    from = (from < 0)
-         ? Math.ceil(from)
-         : Math.floor(from);
-    if (from < 0)
+    from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+    if (from < 0){
       from += len;
-
+    }
     for (; from < len; from++)
     {
-      if (from in this &&
-          this[from] === elt)
+      if (from in this && this[from] === elt){
         return from;
+      }
     }
     return -1;
   };
