577b187153c9e700bbb2cc878659d2591d94e75a
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-29 07:53:42 +0000

    Refs #5043; Defined .indexOf() for Array on browsers which doesn't support it

--- eea/googlecharts/views/js/palettes.js
+++ eea/googlecharts/views/js/palettes.js
@@ -1,3 +1,28 @@
+//define .indexOf() for Array on browsers which doesn't support it
+
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 function toHex(int_nr){
     var hex = Math.round(int_nr).toString(16);
     if (hex.length < 2){
