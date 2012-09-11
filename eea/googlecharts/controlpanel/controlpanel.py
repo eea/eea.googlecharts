6030083d567fa55bf740549085b928b1b100a807
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-09-11 14:35:02 +0000

    Refs #5449; Implement googlechart settings with daviz control panel

---
+++ eea/googlecharts/controlpanel/controlpanel.py
@@ -0,0 +1,96 @@
+""" Googlecharts Control Panel Section
+"""
+from zope.interface import implements
+from eea.app.visualization.controlpanel.interfaces import IDavizSettings
+from zope.formlib.form import FormFields
+from zope import schema
+from eea.app.visualization.controlpanel.controlpanel import DavizSettings
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+class GooglechartsSettings(DavizSettings):
+    """ Googlecharts Settings Section
+    """
+    implements(IDavizSettings)
+    prefix = 'googlechart'
+    title = 'Googlechart Settings'
+    form_fields = FormFields(
+        schema.Choice(
+            __name__='googlechart.qrcode_position',
+            title=u"QRCode Position",
+            required=True,
+            default='Disabled',
+            vocabulary=
+                'eea.googlecharts.controlpanel.vocabularies.imgPositions',
+            ),
+        schema.Int(
+            __name__='googlechart.qrcode_horizontal_space_for_png_export',
+            title=u'QRCode Horizontal Space For PNG Export',
+            required=False,
+            default=0),
+        schema.Int(
+            __name__='googlechart.qrcode_vertical_space_for_png_export',
+            title=u'QRCode Vertical Space For PNG Export',
+            required=False,
+            default=0),
+        schema.Int(
+            __name__='googlechart.qrcode_size',
+            title=u'QRCode Size',
+            required=False,
+            default=0),
+        schema.Choice(
+            __name__='googlechart.watermark_position',
+            title=u"Watermark Position",
+            required=True,
+            default='Disabled',
+            vocabulary=
+                'eea.googlecharts.controlpanel.vocabularies.imgPositions',
+            ),
+        schema.TextLine(
+            __name__='googlechart.watermark_image',
+            title=u'Watermark Image',
+            required=False),
+        schema.Int(
+            __name__='googlechart.watermark_horizontal_space_for_png_export',
+            title=u'Watermark Horizontal Space For PNG Export',
+            required=False,
+            default=0),
+        schema.Int(
+            __name__='googlechart.watermark_vertical_space_for_png_export',
+            title=u'Watermark Vertical Space For PNG Export',
+            required=False,
+            default=0),
+
+    )
+
+GooglechartsSettingsFactory = GooglechartsSettings()
+
+def img_positions_vocabulary(context):
+    """Vocabulary with available positions for watermark and qr code
+    """
+    terms = []
+    terms.append(
+        SimpleTerm(
+            value='Disabled',
+            token='Disabled',
+            title='Disabled'))
+    terms.append(
+        SimpleTerm(
+            value='Top Left',
+            token='Top Left',
+            title='Top Left'))
+    terms.append(
+        SimpleTerm(
+            value='Top Right',
+            token='Top Right',
+            title='Top Right'))
+    terms.append(
+        SimpleTerm(
+            value='Bottom Left',
+            token='Bottom Left',
+            title='Bottom Left'))
+    terms.append(
+        SimpleTerm(
+            value='Bottom Right',
+            token='Bottom Right',
+            title='Bottom Right'))
+    return SimpleVocabulary(terms)
