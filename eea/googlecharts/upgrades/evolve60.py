bf9dd1e8ddac557b4d70d443653f9b96c8f24111
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-09-19 12:44:54 +0000

    Refs #5449; Implemented upgrade tool

---
+++ eea/googlecharts/upgrades/evolve60.py
@@ -0,0 +1,45 @@
+"""Settings upgrade
+"""
+import logging
+logger = logging.getLogger("eea.app.visualization.upgrades")
+
+def migrate_settings(context):
+    """ Migrate settings to portal_davizsettings
+    """
+
+    old_settings = context.portal_properties.site_properties
+    new_settings = context.portal_davizsettings.settings
+
+    migration_mapping = (
+            ('QRCode_Position', 
+            'googlechart.qrcode_position',
+            'Top Left'),
+            ('QRCode_Vertical_Space_For_PNG_Export',
+            'googlechart.qrcode_vertical_space_for_png_export',
+            0),
+            ('QRCode_Horizontal_Space_For_PNG_Export',
+            'googlechart.qrcode_horizontal_space_for_png_export',
+            0),
+            ('Watermark_Image',
+            'googlechart.watermark_image',
+            ''),
+            ('Watermark_Position',
+            'googlechart.watermark_position',
+            'Bottom Right'),
+            ('Watermark_Vertical_Space_For_PNG_Export',
+            'googlechart.watermark_vertical_space_for_png_export',
+            0),
+            ('Watermark_Horizontal_Space_For_PNG_Export',
+            'googlechart.watermark_horizontal_space_for_png_export',
+            0),
+            ('QRCode_Size',
+            'googlechart.qrcode_size',
+            70))
+
+    for setting in migration_mapping:
+        logger.info('Migrating ' + setting[0])
+        new_settings[setting[1]] = old_settings.getProperty(setting[0],
+                                                            setting[2])
+        old_settings._delProperty(setting[0])
+
+    old_settings._delProperty('watermark_positions')
