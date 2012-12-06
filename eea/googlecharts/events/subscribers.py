9d5ec43152ea1f9fbe35a036415dddfb615b1e8d
Alin Voinea <alin@eaudeweb.ro>
2012-12-06 13:08:51 +0200

    Refs #9572 - Fix dashboard settings update when deleting charts.

--- eea/googlecharts/events/subscribers.py
+++ eea/googlecharts/events/subscribers.py
@@ -7,11 +7,6 @@ from eea.app.visualization.interfaces import IVisualizationConfig
 def update_dashboards(context, evt):
     """ Update dashboard charts properties
     """
-    voc = queryUtility(IVocabularyFactory,
-                       name=u'eea.googlecharts.vocabularies.charts')
-
-    settings = dict((term.value, term.title) for term in voc(context))
-
     mutator = queryAdapter(context, IVisualizationConfig)
     view = mutator.view('googlechart.googledashboards', {})
 
@@ -19,13 +14,17 @@ def update_dashboards(context, evt):
     if not dashboards:
         return
 
+    voc = queryUtility(IVocabularyFactory,
+                       name=u'eea.googlecharts.vocabularies.charts')
+    settings = dict((term.value, term.title) for term in voc(context))
+
     changed = False
     for dashboard in dashboards:
         widgets = dashboard.get('widgets', [])
         if not widgets:
             continue
 
-        for widget in widgets:
+        for widget in widgets[:]:
             name = widget.get('name', '')
             if name not in settings:
                 widgets.remove(widget)
