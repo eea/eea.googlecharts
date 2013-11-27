ddc9e98a6a93bef0e4ae15a5c4e22f01a82240d3
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2013-11-27 16:35:54 +0200

    Refs #17453; Added condition for inline resize javascript

--- eea/googlecharts/views/js/inline_resize.js
+++ eea/googlecharts/views/js/inline_resize.js
@@ -473,28 +473,3 @@ jQuery.fn.EEAChartResizer = function(){
         var chartResizer = new DavizInlineResizer.ChartResizer(jQuery(this));
     });
 };
-
-jQuery(document).ready(function($){
-    var resizeButton = "<a class='standardButton googlechart-inline-resize'>Resize chart</div>";
-
-    jQuery.each(jQuery(".embedded-daviz-visualization .standardButton"), function(){
-        if ((jQuery(this).closest("dd").find("div.figure-chart-live").length > 0) &&
-            (jQuery(this).closest("dd").find("div.isChart").length > 0)) {
-            jQuery(this).after(resizeButton);
-        }
-    });
-
-    jQuery("a.googlechart-inline-resize").click(function(){
-        var chartToResize;
-        jQuery.each(jQuery(this).closest(".embedded-daviz-visualization").find(".googlechart_dashboard"), function(idx, chart){
-            if (jQuery(chart).is(":visible")){
-                chartToResize = jQuery(chart);
-            }
-        });
-        chartToResize.EEAChartResizer();
-    });
-
-    jQuery(document).delegate(".googlechart_dashboard", DavizInlineResizer.Events.charts.resized, function(evt, value){
-        //TODO: do the save
-    });
-});
