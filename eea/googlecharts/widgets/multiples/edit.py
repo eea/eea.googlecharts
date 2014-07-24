65de64370d398720e509d9624c248d7b29b40534
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2014-07-24 17:28:32 +0300

    Refs #20495; Started implementing new widget type for small multiple charts

---
+++ eea/googlecharts/widgets/multiples/edit.py
@@ -0,0 +1,45 @@
+""" Edit
+"""
+from zope.component import queryUtility
+from zope.formlib.form import Fields
+from zope.schema.interfaces import IVocabularyFactory
+from eea.googlecharts.widgets.multiples.interfaces import IAdd
+from eea.googlecharts.widgets.multiples.interfaces import IEdit
+from eea.googlecharts.widgets.edit import AddForm, EditForm
+
+
+class Add(AddForm):
+    """ Add widget to dashboard
+    """
+    form_fields = Fields(IAdd)
+
+    def prepare(self, data):
+        """ Update data dict
+        """
+        data = super(Add, self).prepare(data)
+
+        voc = queryUtility(IVocabularyFactory,
+                           name=u'eea.googlecharts.vocabularies.multiples')
+        for term in voc(self.context):
+            if term.value == data.get('name', ''):
+                data['title'] = term.title
+                data['path'] = term.token
+        return data
+
+class Edit(EditForm):
+    """ Edit dashboard widget
+    """
+    form_fields = Fields(IEdit)
+
+    def prepare(self, data):
+        """ Update data dict
+        """
+        data = super(Edit, self).prepare(data)
+
+        voc = queryUtility(IVocabularyFactory,
+                           name=u'eea.googlecharts.vocabularies.multiples')
+        for term in voc(self.context):
+            if term.value == data.get('name', ''):
+                data['title'] = term.title
+                data['path'] = term.token
+        return data
---
+++ eea/googlecharts/widgets/multiples/interfaces.py
@@ -0,0 +1,25 @@
+""" Widget interfaces
+"""
+from zope import schema
+from zope.interface import Interface
+from eea.googlecharts.config import EEAMessageFactory as _
+
+class IAdd(Interface):
+    """ Widget add schema
+    """
+    name = schema.Choice(
+        title=_(u"Chart"),
+        description=_(u"Select chart"),
+        required=True,
+        vocabulary=u"eea.googlecharts.vocabularies.multiples.add"
+    )
+
+class IEdit(Interface):
+    """ Widget edit schema
+    """
+    name = schema.Choice(
+        title=_(u"Chart"),
+        description=_(u"Select chart"),
+        required=True,
+        vocabulary=u"eea.googlecharts.vocabularies.multiples.edit"
+    )
