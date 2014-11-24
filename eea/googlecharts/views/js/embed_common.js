3c6f37307fdf2521c14f1d7e846784e4c10546c6
ichim-david <ichim.david@gmail.com>
2014-11-24 18:17:51 +0200

    Refs #21467; embed_chart ux improvements:
    - Added show and hide icons to the share-spritesheets image
    - Moved share logic to embed_common since it will be used also by the
      dashboard embed. Better caching and debugging is also available now that
      the js is moved out of the template
    - Added functionality that shows and hides the actions div in order to
      avoid content overlap on chart hover
    - Fixed multiple chart embed code

--- eea/googlecharts/views/js/embed_common.js
+++ eea/googlecharts/views/js/embed_common.js
@@ -1,3 +1,4 @@
+/* @flow */
 google.load('visualization', '1.0', {packages: ['controls']});
 
 function putImageDivInPosition(div_id, position, vhash){
@@ -23,3 +24,46 @@ function putImageDivInPosition(div_id, position, vhash){
         jQuery("#" + div_id).addClass("googlechart_right_image");
     }
 }
+
+jQuery(function($){
+    $('.googlecharts-embed-dialog').dialog({ autoOpen: false, width: '80%'});
+    var share_dialog = $('#share-dialog');
+    var embed_url = document.location.href;
+    debugger;
+    var doc = $(document);
+    var doc_height = doc.height();
+    var doc_width = doc.width();
+    var iframeCode = '<iframe width=\'' + doc_width  + '\' height=\'' + doc_height + '\' src=\'' + embed_url + '\'></iframe>';
+    share_dialog.find('.share-dialog-text').text(iframeCode);
+    $('.open-dialog').click(function(ev) {
+        $($(this).attr('data-dialog')).dialog('open');
+        ev.preventDefault();
+    });
+    var embed_obj = window.EEAGoogleCharts.embed;
+    var hash = embed_obj.hash;
+    var baseurl_uri = window.encodeURIComponent(embed_obj['baseurl_' + hash] + '/#tab-' + embed_obj['chart_id_' + hash]);
+    var chart_share = $('.googlechart-share');
+    var chart_share_btn = $('.googlechart-share-btn');
+    doc.hover(function() {
+        chart_share_btn.fadeIn();
+    }, function() {
+        chart_share_btn.fadeOut();
+    });
+    chart_share_btn.click(function(ev){
+        ev.preventDefault();
+        var self = $(this).find('span');
+        chart_share.toggle("slide", { direction: "toggle" }, 500, function(){
+            self.toggleClass('sprite-show').toggleClass('sprite-hide');
+        });
+    });
+
+    var enlarge_button = $('.googlechart-enlarge');
+    enlarge_button.attr('href', embed_url);
+    // show the enlarge button only if we are inside of the iframe
+    if(window === window.top) {
+        enlarge_button.hide();
+    }
+    $('.share-button').each(function(){
+        this.href = this.href + baseurl_uri;
+    });
+});
