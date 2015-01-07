3771a99ea7c7aa4e527f1aa36260a1243f47c03a
ichim-david <ichim.david@gmail.com>
2015-01-07 17:21:08 +0200

    Refs #22337; Changed the following:
    - Removed top and bottom padding of share box which brought box sizing
      differences for embed charts
    - Loaded the share box logic only after the googlechart draw logic is
      completed both in embed chart and embed dashboard
    - No longer load the sharing options within inline charts
    - Share links now open in target blank in order to avoid browser
      blocking when attempting to click the links especially on Chrome

--- eea/googlecharts/views/js/embed_common.js
+++ eea/googlecharts/views/js/embed_common.js
@@ -26,41 +26,43 @@ function putImageDivInPosition(div_id, position, vhash){
 }
 
 jQuery(function($){
-    $('.googlecharts-embed-dialog').dialog({ autoOpen: false, width: '80%'});
-    var share_dialog = $('#share-dialog');
-    var embed_url = document.location.href;
     var doc = $(document);
-    var doc_height = doc.height();
-    var doc_width = doc.width();
-    var iframeCode = '<iframe width=\'' + doc_width  + '\' height=\'' + doc_height + '\' src=\'' + embed_url + '\'></iframe>';
-    share_dialog.find('.share-dialog-text').text(iframeCode);
-    $('.open-dialog').click(function(ev) {
-        $($(this).attr('data-dialog')).dialog('open');
-        ev.preventDefault();
-    });
-    if (!window.EEAGoogleCharts) {
-        return;
-    }
-    var embed_obj = window.EEAGoogleCharts.embed;
-    var hash = embed_obj.hash;
-    var baseurl_uri = window.encodeURIComponent(embed_obj['baseurl_' + hash] + '/#tab-' + embed_obj['chart_id_' + hash]);
-    var chart_share = $('.googlechart-share');
-    chart_share.parent().appendTo("#googlechart_bottom_images_" + hash);
-    $(".googlechart_left_image").find('img').appendTo("#share-dialog");
-    $(".googlechart-datasources-info").appendTo('.googlechart-notes');
-    doc.hover(function() {
-        chart_share.fadeIn();
-    }, function() {
-        chart_share.fadeOut();
-    });
+    doc.bind('googlecharts.embed.ready', function() {
+        $('.googlecharts-embed-dialog').dialog({ autoOpen: false, width: '80%'});
+        var share_dialog = $('#share-dialog');
+        var embed_url = this.location.href;
+        var doc_height = doc.height();
+        var doc_width = doc.width();
+        var iframeCode = '<iframe width=\'' + doc_width  + '\' height=\'' + doc_height + '\' src=\'' + embed_url + '\'></iframe>';
+        share_dialog.find('.share-dialog-text').text(iframeCode);
+        $('.open-dialog').click(function(ev) {
+            $($(this).attr('data-dialog')).dialog('open');
+            ev.preventDefault();
+        });
+        if (!window.EEAGoogleCharts) {
+            return;
+        }
+        var embed_obj = window.EEAGoogleCharts.embed;
+        var hash = embed_obj.hash;
+        var baseurl_uri = window.encodeURIComponent(embed_obj['baseurl_' + hash] + '/#tab-' + embed_obj['chart_id_' + hash]);
+        var chart_share = $('.googlechart-share');
+        chart_share.parent().appendTo("#googlechart_bottom_images_" + hash);
+        $(".googlechart_left_image").find('img').appendTo("#share-dialog");
+        $(".googlechart-datasources-info").appendTo('.googlechart-notes');
+        doc.hover(function() {
+            chart_share.fadeIn();
+        }, function() {
+            chart_share.fadeOut();
+        });
 
-    var enlarge_button = $('.googlechart-enlarge');
-    enlarge_button.attr('href', embed_url);
-    // show the enlarge button only if we are inside of the iframe
-    if(window === window.top) {
-        enlarge_button.hide();
-    }
-    $('.share-button').each(function(){
-        this.href = this.href + baseurl_uri;
+        var enlarge_button = $('.googlechart-enlarge');
+        enlarge_button.attr('href', embed_url);
+        // show the enlarge button only if we are inside of the iframe
+        if(window === window.top) {
+            enlarge_button.hide();
+        }
+        $('.share-button').each(function(){
+            this.href = this.href + baseurl_uri;
+        });
     });
 });
